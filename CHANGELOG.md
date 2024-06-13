58966a86107f9e5705a2d139ba08d5eaef98e0f0
UltimatChamp <sohamguptta2009@gmail.com>
2024-06-13 20:29:35 +0530

    bump: 1.0.13
    * moved to stonecutter which means 1.20-1.21 in the same branch
    * temp. changed release script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,4 @@
-**Full Changelog: [1.0.11+1.20.6...1.0.12+1.21](https://github.com/UltimatChamp/FabricBetterGrass/compare/1.0.11+1.20.6...1.0.12+1.21)**
+**Full Changelog: [1.0.12+1.21...1.0.13+1.21rc1](https://github.com/UltimatChamp/FabricBetterGrass/compare/1.0.12+1.21...1.0.13+1.21rc1)**
 
-- Port to **1.21** 🥳!
-  - As of publishing this release, **YACL** for 1.21 isn't available on **CurseForge** and **Modrinth**! Use [**👉this👈**](https://maven.isxander.dev/releases/dev/isxander/yet-another-config-lib/3.5.0+1.21-fabric/yet-another-config-lib-3.5.0+1.21-fabric.jar) for the time being!
-- Using vanilla translation strings, wherever possible.
-  - Some **tooltips** and **options** still require to be translated!
+- Moved to **Stonecutter**!
+  - No in-game changes!
--- src/main/java/dev/ultimatchamp/bettergrass/mixin/FabricBetterGrassModelLoaderMixin.java
+++ src/main/java/dev/ultimatchamp/bettergrass/mixin/FabricBetterGrassModelLoaderMixin.java
@@ -5,6 +5,7 @@ import dev.ultimatchamp.bettergrass.config.FabricBetterGrassConfig;
 import net.minecraft.client.render.model.ModelLoader;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,39 +14,60 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
+import java.util.Set;
 
 @Mixin(ModelLoader.class)
 public class FabricBetterGrassModelLoaderMixin {
     @Shadow
     @Final
+    //? if >1.20.6 {
     private Map<ModelIdentifier, UnbakedModel> modelsToBake;
+    //?} else
+    /*private Map<Identifier, UnbakedModel> unbakedModels;
 
+    @Shadow
+    @Final
+    private Set<Identifier> modelsToLoad;*/
+
+    //? if >1.20.6 {
     @Inject(method = "addModelToBake", at = @At("HEAD"), cancellable = true)
     private void onAddModelToBake(ModelIdentifier id, UnbakedModel unbakedModel, CallbackInfo ci) {
+    //?} else
+    /*@Inject(method = "putModel", at = @At("HEAD"), cancellable = true)
+    private void onPutModel(Identifier id, UnbakedModel unbakedModel, CallbackInfo ci) {*/
         if (id instanceof ModelIdentifier modelId) {
             if (!modelId.getVariant().equals("inventory")) {
                 FabricBetterGrassConfig.instance().grassBlocks.forEach(s -> {
                     if (modelId.toString().startsWith(s.split("\\[")[0]) && !modelId.toString().contains("snowy=true")) {
                         var newModel = new FabricBetterGrassUnbakedModel(unbakedModel);
+                        //? if >1.20.6 {
                         this.modelsToBake.put(id, newModel);
+                        //?} else
+                        /*this.unbakedModels.put(id, newModel);
+                        this.modelsToLoad.addAll(newModel.getModelDependencies());*/
                         ci.cancel();
                     }
-                });
-
-                if (FabricBetterGrassConfig.instance().snowy) {
-                    FabricBetterGrassConfig.instance().grassBlocks.forEach(s -> {
+                    if (FabricBetterGrassConfig.instance().snowy) {
                         if (modelId.toString().startsWith(s.split("\\[")[0]) && modelId.toString().contains("snowy=true")) {
                             var newModel = new FabricBetterGrassUnbakedModel(unbakedModel);
+                            //? if >1.20.6 {
                             this.modelsToBake.put(id, newModel);
+                            //?} else
+                            /*this.unbakedModels.put(id, newModel);
+                            this.modelsToLoad.addAll(newModel.getModelDependencies());*/
                             ci.cancel();
                         }
-                    });
-                }
+                    }
+                });
 
                 if (FabricBetterGrassConfig.instance().dirtPaths) {
                     if (modelId.toString().startsWith("minecraft:dirt_path".split("\\[")[0])) {
                         var newModel = new FabricBetterGrassUnbakedModel(unbakedModel);
+                        //? if >1.20.6 {
                         this.modelsToBake.put(id, newModel);
+                        //?} else
+                        /*this.unbakedModels.put(id, newModel);
+                        this.modelsToLoad.addAll(newModel.getModelDependencies());*/
                         ci.cancel();
                     }
                 }
@@ -53,7 +75,11 @@ public class FabricBetterGrassModelLoaderMixin {
                 if (FabricBetterGrassConfig.instance().farmLands) {
                     if (modelId.toString().startsWith("minecraft:farmland".split("\\[")[0])) {
                         var newModel = new FabricBetterGrassUnbakedModel(unbakedModel);
+                        //? if >1.20.6 {
                         this.modelsToBake.put(id, newModel);
+                        //?} else
+                        /*this.unbakedModels.put(id, newModel);
+                        this.modelsToLoad.addAll(newModel.getModelDependencies());*/
                         ci.cancel();
                     }
                 }
